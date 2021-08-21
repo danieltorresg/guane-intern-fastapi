a080a84893c3dc591c31d88e7edc0684f7515568
Daniel <daniel.torresg@udea.edu.co>
2021-08-21 10:47:46 -0500

    routing user endpoints

--- app/api/api.py
+++ app/api/api.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
-from app.api.endpoints import dog
+from app.api.endpoints import dog, user
 
 api_router = APIRouter()
 api_router.include_router(dog.router, prefix="/dogs", tags=["dog"])
+api_router.include_router(user.router, prefix="/users", tags=["user"])
